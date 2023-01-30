ba322b4164287c07874fd72439273a622b576224
Nguyen Khac Vu <vunkph19573@fpt.edu.vn>
2023-01-30 15:36:29 +0700

    p6_challange1

--- session9/challenge1/index.js
+++ session9/challenge1/index.js
@@ -50,9 +50,15 @@ console.log(gk, fieldPlayers);
 const allPlayers = [...player1, ...player2];
 console.log(allPlayers);
 //4.
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Periscic'];
 console.log(players1Final);
 //5.
 const { odds: { team1, x: draw, team2 },
 } = game;
 console.log(team1, draw, team2);
+//6.
+const printGoals = function (...players) {
+    console.log(players);
+    console.log(`${players.length} goals were scored`);
+};
+printGoals(...game.scored);
